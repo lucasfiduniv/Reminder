32c4e32fa6a9405f3b65b3d06d864eb9dcd32092
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2025-02-22 15:30:34 -0300

    fix: 🐛 terminando configuracao basica do controller do splash view

--- Reminder/Sources/Features/Splash/SplashViewController.swift
+++ Reminder/Sources/Features/Splash/SplashViewController.swift
@@ -9,8 +9,28 @@ import Foundation
 import UIKit
 
 class SplashViewController: UIViewController{
+    let contentView = SplashView()
     override func viewDidLoad() {
-        <#code#>
+        super.viewDidLoad()
+        
+        let contentView = SplashView()
+        setup()
     }
+    private func setup(){
+        self.view.addSubview(contentView)
+        
+        setupConstraints()
+    }
+    private func setupConstraints(){
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: view.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+    }
+   
     
 }
