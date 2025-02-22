d99800a604a1ead7e7ad07cb5d237eb4884624ff
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2025-02-22 15:34:17 -0300

    fix: 🐛 testando colocar fundo azul para ver se esta funcionando

--- Reminder/Sources/Features/Splash/SplashView.swift
+++ Reminder/Sources/Features/Splash/SplashView.swift
@@ -14,6 +14,7 @@ class SplashView: UIView{
         setupUI()
     }
     private func setupUI(){
+        self.backgroundColor = .blue
         setupConstraints()
     }
     private func setupConstraints(){
