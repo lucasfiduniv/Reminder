44c6f86cf297969efd9f28d9d7b7c2a16abefe85
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2025-02-22 15:30:48 -0300

    feat: ✨ criando view do splash

---
+++ Reminder/Sources/Features/Splash/SplashView.swift
@@ -0,0 +1,26 @@
+//
+//  SplashView.swift
+//  Reminder
+//
+//  Created by Lucas Gabriel Fiduniv on 22/02/25.
+//
+
+import Foundation
+import UIKit
+
+class SplashView: UIView{
+    init(){
+        super.init(frame: .zero)
+        setupUI()
+    }
+    private func setupUI(){
+        setupConstraints()
+    }
+    private func setupConstraints(){
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
