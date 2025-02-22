ca3fb4386e5e709f01ddba90a34914a327dc669d
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2025-02-22 15:15:31 -0300

    feat: ✨ criando funcao de loading de carregamento do app

---
+++ Reminder/Sources/Features/Splash/SplashViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SplashViewController.swift
+//  Reminder
+//
+//  Created by Lucas Gabriel Fiduniv on 22/02/25.
+//
+
+import Foundation
+import UIKit
+
+class SplashViewController: UIViewController{
+    override func viewDidLoad() {
+        <#code#>
+    }
+    
+}
