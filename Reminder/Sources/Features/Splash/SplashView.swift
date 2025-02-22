5bd4d6a331d2d82e3a5d02422a792f7a86a482f6
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2025-02-22 20:21:41 -0300

    fix: 🐛 removendo parte do texto no title que nao existe mais

--- Reminder/Sources/Features/Splash/SplashView.swift
+++ Reminder/Sources/Features/Splash/SplashView.swift
@@ -16,36 +16,19 @@ class SplashView: UIView{
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
-    
-    private let titleLogoLabel:UILabel = {
-        let label = UILabel()
-        label.text = "Remind"
-        label.textColor = .white
-        label.font = UIFont.boldSystemFont(ofSize: 32)
-        label.translatesAutoresizingMaskIntoConstraints = false;
-        return label
-    }()
-    
     init(){
         super.init(frame: .zero)
         setupUI()
     }
     private func setupUI(){
         self.addSubview(logoImgView)
-        self.addSubview(titleLogoLabel)
         setupConstraints()
     }
     private func setupConstraints(){
         
         NSLayoutConstraint.activate([
-            titleLogoLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor,constant: 24),
-            titleLogoLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-        
-            
-            logoImgView.widthAnchor.constraint(equalToConstant: 50),
-            logoImgView.heightAnchor.constraint(equalToConstant: 50),
-            logoImgView.trailingAnchor.constraint(equalTo: titleLogoLabel.leadingAnchor, constant: -8),
-            logoImgView.bottomAnchor.constraint(equalTo: titleLogoLabel.bottomAnchor, constant: 0)
+            logoImgView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            logoImgView.centerYAnchor.constraint(equalTo: centerYAnchor)
         ])
     }
     
