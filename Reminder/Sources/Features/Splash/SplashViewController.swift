25a9e8f4c60e73cb3710c1cb9b295518a2089a19
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2025-02-22 21:37:41 -0300

    fix: 🐛 mexendo no splash para adicionar de abrir o campo de login depois de carregar a aplicacao

--- Reminder/Sources/Features/Splash/SplashViewController.swift
+++ Reminder/Sources/Features/Splash/SplashViewController.swift
@@ -8,30 +8,48 @@
 import Foundation
 import UIKit
 
-class SplashViewController: UIViewController{
+class SplashViewController: UIViewController {
     let contentView = SplashView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let _contentView = SplashView()
         setup()
     }
-    private func setup(){
+    
+    private func setup() {
         self.view.addSubview(contentView)
-        self.view.backgroundColor = Colors.primaryRedBase
         self.navigationController?.navigationBar.isHidden = true
+        self.view.backgroundColor = Colors.primaryRedBase
+        
         setupConstraints()
+        setupGesture()
     }
-    private func setupConstraints(){
+    
+    private func setupConstraints() {
         NSLayoutConstraint.activate([
             contentView.topAnchor.constraint(equalTo: view.topAnchor),
             contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
         
         contentView.translatesAutoresizingMaskIntoConstraints = false
     }
-   
     
+    private func setupGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(showLoginBottomSheet))
+        self.view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc
+    private func showLoginBottomSheet() {
+        let loginBottomSheet = LoginBottonSheetViewController()
+        loginBottomSheet.modalPresentationStyle = .overCurrentContext
+        loginBottomSheet.modalTransitionStyle = .crossDissolve
+        self.present(loginBottomSheet, animated: false) {
+            loginBottomSheet.animateShow()
+        }
+    }
+        
 }
