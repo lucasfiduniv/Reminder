da4d6e910b7781d1fde1ca44481adb42816a14e4
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2025-02-23 16:04:45 -0300

    fix: 🐛 mudando todas as strings para buscar do arquivo de strings

--- Reminder/Sources/Features/LoginBottonSheet/LoginBottonSheetView.swift
+++ Reminder/Sources/Features/LoginBottonSheet/LoginBottonSheetView.swift
@@ -12,7 +12,7 @@ class LoginBottonSheetView:UIView{
     
     private let titleLabel:UILabel = {
         let label = UILabel()
-        label.text = "Entre para acessar suas receitas"
+        label.text = "login.label.title".localized
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -27,7 +27,7 @@ class LoginBottonSheetView:UIView{
     
     private let emailTextField: UITextField = {
         let text = UITextField()
-        text.placeholder = "Email@exemplo.com"
+        text.placeholder = "login.email.placeholder".localized
         text.borderStyle = .roundedRect
         text.translatesAutoresizingMaskIntoConstraints = false
         return text
@@ -35,7 +35,7 @@ class LoginBottonSheetView:UIView{
     
     private let passwordTextField: UITextField = {
         let text = UITextField()
-        text.placeholder = "senha"
+        text.placeholder = "login.password.placeholder".localized
         text.isSecureTextEntry = true
         text.borderStyle = .roundedRect
         text.translatesAutoresizingMaskIntoConstraints = false
@@ -44,7 +44,7 @@ class LoginBottonSheetView:UIView{
     
     private let loginButton:UIButton = {
         let button = UIButton(type: .system)
-        button.setTitle("Entrar", for: .normal)
+        button.setTitle("login.button.title".localized, for: .normal)
         button.backgroundColor = Colors.primaryRedBase
         button.layer.cornerRadius = Metrics.tiny
         button.translatesAutoresizingMaskIntoConstraints = false
