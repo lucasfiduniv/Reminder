69a3ee599a87986515918bdbe6221f7bc54fa70f
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2025-02-22 21:11:49 -0300

    fix: 🐛 criando as constraint

--- Reminder/Sources/Features/LoginBottonSheet/LoginBottonSheetView.swift
+++ Reminder/Sources/Features/LoginBottonSheet/LoginBottonSheetView.swift
@@ -71,6 +71,24 @@ class LoginBottonSheetView:UIView{
         setupConstraints()
     }
     private func setupConstraints() {
-        
-    }
-}
+          NSLayoutConstraint.activate([
+              handleArea.topAnchor.constraint(equalTo: self.topAnchor, constant: Metrics.small),
+              handleArea.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+              handleArea.widthAnchor.constraint(equalToConstant: 40),
+              handleArea.heightAnchor.constraint(equalToConstant: 6),
+              
+              titleLabel.topAnchor.constraint(equalTo: handleArea.bottomAnchor, constant: Metrics.medium),
+              titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Metrics.medium),
+              
+              emailTextField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: Metrics.medium),
+              emailTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Metrics.medium),
+              
+              passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: Metrics.medium),
+              passwordTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Metrics.medium),
+              
+              loginButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: Metrics.medium),
+              loginButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Metrics.medium),
+              loginButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -Metrics.medium),
+              loginButton.heightAnchor.constraint(equalToConstant: 50)
+          ])
+      }}
