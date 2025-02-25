eaeb430f457e2fbb8788948c0f41f0caf1eab528
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2025-02-24 21:23:57 -0300

    feat: ✨ criando parte inicial da view model de login

---
+++ Reminder/Sources/Features/LoginBottonSheet/ViewModel/LoginBottomSheetViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  loginBottomSheetViewModel.swift
+//  Reminder
+//
+//  Created by Lucas Gabriel Fiduniv on 24/02/25.
+//
+
+import Foundation
+
+class LoginBottomSheetViewModel {
+    func doAuth(username:String, password: String){
+        
+    }
+}
